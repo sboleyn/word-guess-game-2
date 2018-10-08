65c0063c196423eaf8101fe63e83b79187075834
Samantha Boleyn <Samantha.Boleyn@maaxmicrosoft.com>
2018-10-07 17:22:35 -0700

    upload changes

--- letter.js
+++ letter.js
@@ -1,8 +1,10 @@
 //  *Letter.js**: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
 fucntion Letter (letter){
-    this.mask = '_',
     this.letter = letter,
-    this.guessed = false
+    this.guessed = false,
+    this.reveal = function(){
+
+    }
 } ;
 //  * A string value to store the underlying character for the letter
 
